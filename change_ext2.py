e3dc7ae9946c1ee8ba02a490d76e829e1b60adc5
drakehitch <filin140123@gmail.com>
2022-07-06 20:36:57 +0500

    update: change_ext2.py refactor

--- change_ext2.py
+++ change_ext2.py
@@ -1,11 +1,11 @@
 import os
 
-exts = input("Enter formats, divided by space (example: 'md adoc txt'): ").split()
+exts = tuple(input("Enter formats, divided by space (example: 'md adoc txt'): ").split())
 new_ext = input("Enter wanted extension: ")
 
 for dirname, dirs, files in os.walk("."):
     for filename in files:
-        if filename.endswith(tuple(exts)):
+        if filename.endswith(exts):
             old = dirname + "\\" + filename
             new = dirname + "\\" + filename.split(".")[0] + "." + new_ext
             os.rename(old, new)
